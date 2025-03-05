f1a05b1ee7357fd03c118d18b43eaf6c71770dd3
edgeidr <edge.idr@gmail.com>
2025-03-05 17:29:45 +0800

    fix tailwind

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -3,6 +3,7 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
+	css: ["@/assets/tailwind.css"],
 	modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
 	primevue: {
 		importTheme: { from: "@/primevue/theme", as: "auraTheme" },
