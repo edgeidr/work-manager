581db86a9bda7e46532ee532dd7fb10fc33affb4
edgeidr <edge.idr@gmail.com>
2025-04-18 21:21:07 +0800

    add users resource

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, UsersModule],
 })
 export class AppModule {}
