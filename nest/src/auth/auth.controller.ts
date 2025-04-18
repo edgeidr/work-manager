c486000f1b229da68ae0dae8f64d353e2940842a
edgeidr <edge.idr@gmail.com>
2025-04-18 14:05:37 +0800

    signUp - sample

--- nest/src/auth/auth.controller.ts
+++ nest/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
 	@Post('signup')
-	signUp() {
-		return this.authService.signUp();
+	signUp(@Body() signUpDto: SignUpDto) {
+		return this.authService.signUp(signUpDto);
 	}
 
 	@Post('signin')
--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
 	constructor(private prisma: PrismaService) {}
 
-	signUp() {
-		return 'signup';
+	async signUp(signUpDto: SignUpDto) {
+		const hashedPassword = await hash(signUpDto.password);
+		const user = await this.prisma.user.create({
+			data: {
+				email: signUpDto.email,
+				password: hashedPassword,
+				firstName: signUpDto.firstName,
+				lastName: signUpDto.lastName,
+				isActive: true,
+			},
+			omit: {
+				password: true,
+			},
+		});
+
+		return user;
 	}
 
 	signIn() {
