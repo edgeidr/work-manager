cce1b6207cdfef7529c64794ae377b32b6ee9f46
edgeidr <edge.idr@gmail.com>
2025-04-19 12:46:41 +0800

    Auth E2E Test

--- nest/test/app.e2e-spec.ts
+++ nest/test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
+import { SignUpDto } from '../src/auth/dto/sign-up.dto';
+import { SignInDto } from '../src/auth/dto/sign-in.dto';
 
 describe('App e2e', () => {
 	let app: INestApplication;
@@ -26,6 +28,8 @@ describe('App e2e', () => {
 		prisma = app.get(PrismaService);
 
 		await prisma.cleanDb();
+
+		pactum.request.setBaseUrl('http://localhost:3001');
 	});
 
 	afterAll(() => {
@@ -34,10 +38,80 @@ describe('App e2e', () => {
 
 	describe('Auth', () => {
 		describe('Signup', () => {
-			it.todo('should signup');
+			const url = '/auth/signup';
+			const signUpDto: SignUpDto = {
+				email: 'edge.idr@gmail.com',
+				password: 'password',
+				firstName: 'Ian',
+				lastName: 'Del Rosario',
+			};
+
+			it('should throw if email is empty', () => {
+				const { email, ...incompleteDto } = signUpDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if password is empty', () => {
+				const { password, ...incompleteDto } = signUpDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if firstName is empty', () => {
+				const { firstName, ...incompleteDto } = signUpDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if lastName is empty', () => {
+				const { lastName, ...incompleteDto } = signUpDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if no body is provided', () => {
+				return pactum.spec().post(url).expectStatus(400);
+			});
+
+			it('should signup', () => {
+				const signUpDto: SignUpDto = {
+					email: 'edge.idr@gmail.com',
+					password: 'password',
+					firstName: 'Ian',
+					lastName: 'Del Rosario',
+				};
+
+				return pactum.spec().post(url).withBody(signUpDto).expectStatus(201);
+			});
 		});
+
 		describe('Signin', () => {
-			it.todo('should signin');
+			const url = '/auth/signin';
+			const signInDto: SignInDto = {
+				email: 'edge.idr@gmail.com',
+				password: 'password',
+			};
+
+			it('should throw if email is empty', () => {
+				const { email, ...incompleteDto } = signInDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if password is empty', () => {
+				const { password, ...incompleteDto } = signInDto;
+
+				return pactum.spec().post(url).withBody(incompleteDto).expectStatus(400);
+			});
+
+			it('should throw if no body is provided', () => {
+				return pactum.spec().post(url).expectStatus(400);
+			});
+
+			it('should signin', () => {
+				return pactum.spec().post(url).withBody(signInDto).expectStatus(201);
+			});
 		});
 	});
 
