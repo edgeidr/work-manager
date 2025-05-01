39d41be3efd732d32c2d772e32853e77066ac372
edgeidr <edge.idr@gmail.com>
2025-05-02 00:22:46 +0800

    store user in shared-auth-test

--- nest/test/utils/shared-auth-test.ts
+++ nest/test/utils/shared-auth-test.ts
@@ -3,8 +3,36 @@ import * as pactum from 'pactum';
 
 type HttpMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
-export const itShouldThrowIfUnauthenticated = (method: HttpMethod, url: string) => {
+const itShouldThrowIfUnauthenticated = (method: HttpMethod, url: string) => {
 	it('should throw if unauthenticated', () => {
 		return pactum.spec()[method](url).expectStatus(HttpStatus.UNAUTHORIZED);
 	});
 };
+
+const signInAsSuperuser = () => {
+	describe('Signin', () => {
+		const url = '/auth/signin';
+		const superadminCredentials = {
+			email: 'superadmin@gmail.com',
+			password: 'p@ssword',
+		};
+
+		it('should signin', () => {
+			return pactum
+				.spec()
+				.post(url)
+				.withBody(superadminCredentials)
+				.expectStatus(HttpStatus.OK)
+				.expectBodyContains('accessToken')
+				.expectBodyContains('refreshToken')
+				.expectBodyContains('deviceId')
+				.expectBodyContains('user')
+				.stores('accessToken', 'accessToken')
+				.stores('refreshToken', 'refreshToken')
+				.stores('deviceId', 'deviceId')
+				.stores('user', 'user');
+		});
+	});
+};
+
+export { itShouldThrowIfUnauthenticated, signInAsSuperuser };
