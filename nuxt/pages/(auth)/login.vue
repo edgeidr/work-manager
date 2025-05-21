597ec79587089ba2c27aa29a662e6b25a0877eaf
edgeidr <edge.idr@gmail.com>
2025-05-21 15:51:53 +0800

    fix login (no validation message)

--- nuxt/pages/(auth)/login.vue
+++ nuxt/pages/(auth)/login.vue
@@ -6,23 +6,23 @@
 				<p class="text-muted-color">Please enter your details here.</p>
 			</div>
 
-			<form v-focustrap>
+			<form v-focustrap @submit.prevent="onSubmit">
 				<div class="space-y-4">
 					<div>
 						<label>Username</label>
-						<InputText class="mt-1" fluid />
+						<InputText v-model="form.email" class="mt-1" fluid />
 					</div>
 
 					<div>
 						<label>Password</label>
-						<Password :feedback="false" class="mt-1" fluid />
+						<Password v-model="form.password" :feedback="false" class="mt-1" fluid />
 
 						<div class="my-2.5 text-right">
 							<Button label="Forgot Password?" variant="link" class="!p-0" size="small" />
 						</div>
 					</div>
 
-					<Button label="Sign In" fluid />
+					<Button type="submit" label="Sign In" fluid />
 
 					<div class="flex items-center">
 						<div class="h-px flex-1 border border-surface-500"></div>
@@ -55,4 +55,28 @@
 
 <script setup lang="ts">
 	definePageMeta({ layout: "auth" });
+
+	const user = useLocalStorage<User>("user", null);
+	const form = ref({
+		email: "",
+		password: "",
+	});
+
+	const { execute: signIn, data: auth } = await useCustomFetch<User>("/auth/signin", {
+		immediate: false,
+		watch: false,
+		method: "POST",
+		body: form,
+	});
+
+	const { execute: getMe, data } = await useCustomFetch("/users/me", {
+		immediate: false,
+		watch: false,
+	});
+
+	const onSubmit = async () => {
+		await signIn();
+		user.value = auth.value;
+		await getMe();
+	};
 </script>
