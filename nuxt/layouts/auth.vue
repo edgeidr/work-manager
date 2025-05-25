788b8d52e1fecb59cf24b6442c578d41990b7ce6
edgeidr <edge.idr@gmail.com>
2025-05-25 16:24:12 +0800

    modify auth layout

--- nuxt/layouts/auth.vue
+++ nuxt/layouts/auth.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="h-screen w-screen bg-surface-400 bg-[url('/assets/images/login-banner.jpg')] bg-cover bg-center">
 		<div class="flex size-full items-center justify-center sm:justify-end">
-			<div class="m-8 h-fit rounded-lg bg-surface-800/60 px-8 py-14 shadow-lg backdrop-blur-lg">
+			<div class="m-8 h-fit w-full max-w-96 rounded-lg bg-surface-800/60 px-8 py-14 shadow-lg backdrop-blur-lg">
 				<slot />
 			</div>
 		</div>
