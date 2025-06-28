650dcfbf833d320babe176e11c7c6876d02b8a41
edgeidr <edge.idr@gmail.com>
2025-06-29 00:19:44 +0800

    move jwt to tokens module instead of auth module

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { OtpsModule } from '../otps/otps.module';
 import { UsersModule } from '../users/users.module';
 import { TokensModule } from '../tokens/tokens.module';
 
 @Module({
-	imports: [JwtModule.register({}), UsersModule, TokensModule, OtpsModule],
+	imports: [UsersModule, TokensModule, OtpsModule],
 	controllers: [AuthController],
 	providers: [AuthService, JwtStrategy],
 })
--- nest/src/tokens/tokens.module.ts
+++ nest/src/tokens/tokens.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
 	providers: [TokensService],
 	exports: [TokensService],
+	imports: [JwtModule.register({})],
 })
 export class TokensModule {}
